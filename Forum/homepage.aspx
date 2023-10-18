d74ae3ce0a20c09477604843beff3b34c21a3719
Daxrajsinh <daxrajsinh16@gmail.com>
2023-10-18 15:34:49 +0530

    final commit

---
+++ Forum/homepage.aspx
@@ -0,0 +1,143 @@
+﻿
+<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="Forum.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+  
+    <style>
+        .card {
+            border: 1px solid #ccc;
+            margin: 20px 0;
+            border-radius: 10px;
+        }
+       .liked {
+        color: mediumpurple !important;
+    }
+       .rounded-title {
+       background-color: mediumpurple; 
+       color: white; 
+       border-radius: 20px; 
+       padding: 5px 15px; 
+       }
+      
+     .custom-button {
+      border: 2px solid mediumpurple; 
+     }
+
+    .custom-button:hover,
+    .custom-button:active,
+    .custom-button:focus {
+        color: white !important; 
+        background-color: mediumpurple ;
+        border-color: mediumpurple ;
+    }
+  
+.custom-button:hover .bx-chat {
+    color: white !important; /* White icon color on hover */
+}
+.custom-button:hover .bx-like {
+    color: white !important; /* White icon color on hover */
+}
+
+  .post-image { 
+        overflow: hidden; 
+        transition: transform 0.3s;
+        z-index: -1;
+    }
+     .post-image:hover {
+        transform: scale(1.2); 
+        z-index: 5;
+    }
+
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="col-md-8 offset-md-2">
+    <asp:Repeater ID="PostRepeater" runat="server" OnItemCommand="PostRepeater_ItemCommand">
+        <ItemTemplate>
+            <div class="card mt-4" style="border: solid 1px;border-color: rgb(167, 167, 167); margin: 2% 1%">
+                <div class="card-header" style="background-color: #ebf2f2; border: dotted 1px">
+                   Posted By:
+                    <asp:Label runat="server" Text='<%# Eval("Username")%>'></asp:Label>
+                   
+                        <p class="mb-0">
+                            <small class="text-muted"><asp:Label runat="server" Text='<%# Eval("PostDate") %>'></asp:Label></small>
+                        </p>
+                    <!-- <asp:Panel ID="adminpanel" runat="server" Visible="true"> -->
+                    <% if (Session["role"].ToString()=="admin")
+                    {   
+                    %>
+                        <div class="d-flex justify-content-between" style="font-size: x-large">
+                            <asp:LinkButton ID="EditLink" OnClick="EditLink_Click" CommandArgument='<%#Eval("Id")%>' style="text-decoration: none; color: black" runat="server">
+                                <i class='bx bx-edit-alt'></i>
+                            </asp:LinkButton>
+                            <asp:LinkButton ID="DeleteLink" OnClientClick="return confirm('Are you sure you want to delete this post?');" OnClick="DeleteLink_Click" CommandArgument='<%#Eval("Id")%>' style="text-decoration: none; color: rgb(133, 2, 2)" runat="server">
+                                <i class='bx bxs-trash'></i>
+                            </asp:LinkButton>
+                        </div>
+                    <% 
+                    }
+                    %>
+                   
+                   <!-- </asp:Panel> -->      
+                </div>
+                <div class="card-body" style="background-color:#fcffff">
+                    <h6 class="card-title ">
+                        <asp:Label runat="server" CssClass="rounded-title" Text='<%# Eval("PostTitle")%>'></asp:Label>
+                    </h6>
+                  
+                  <br />
+                    <asp>
+
+                    </asp>
+    <asp:PlaceHolder runat="server" Visible='<%# IsImageAvailable(Eval("PostPic")) %>'>
+    <div class="row">
+        <div class="col-md-4">
+            <asp:Image runat="server" ImageUrl='<%# GetPostPicUrl(Eval("PostPic")) %>'
+                CssClass="img-fluid rounded shadow post-image" />
+        </div>
+        <div class="col-md-8">
+            <p class="card-content">
+                <asp:Label runat="server" CssClass="card-text" Text='<%# Eval("PostContent")%>'></asp:Label>
+            </p>
+            <!-- Other content related to your blog post can go here -->
+        </div>
+    </div>
+</asp:PlaceHolder>
+
+<asp:PlaceHolder runat="server" Visible='<%# !IsImageAvailable(Eval("PostPic")) %>'>
+    <div class="row">
+        <div class="col-md-8">
+            <p class="card-content">
+                <asp:Label runat="server" CssClass="card-text" Text='<%# Eval("PostContent")%>'></asp:Label>
+            </p>
+            <!-- Other content related to your blog post can go here -->
+        </div>
+    </div>
+</asp:PlaceHolder>
+
+
+
+                     <!-- Display user's uploaded image -->
+                  <br />
+                   <div class="btn-group mt-2" role="group" aria-label="Second group">
+    <asp:LinkButton ID="ReplyLink" OnClick="ReplyLink_Click" CommandArgument='<%#Eval("Id")%>' CssClass="btn btn-outline-primary custom-button" runat="server">
+        <i class="bx bx-chat" style="color: mediumpurple;"></i>
+        &nbsp;
+      
+    </asp:LinkButton>   &nbsp;
+                       <asp:LinkButton ID="LikeLink" OnClick="LikeLink_Click" CommandArgument='<%#Eval("Id")%>' CssClass="btn btn-outline-primary custom-button" runat="server">
+            <i class="bx bx-like" style="font-size: x-large; color:mediumpurple"></i>
+            <span class="like-count" style="color:mediumpurple">
+    <%# Eval("LikeCount") %>
+</span>
+
+        </asp:LinkButton>
+</div>
+
+                </div>
+            </div>
+
+        </ItemTemplate>
+        
+    </asp:Repeater>
+             </div>
+</asp:Content>
---
+++ Forum/homepage.aspx.cs
@@ -0,0 +1,184 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Forum
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        public int checkadmin;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["role"] == null)
+            {
+                Session["role"] = "";
+            }
+            RepeterData();
+            PostRepeater.DataBind();
+        }
+        void RepeterData()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(strcon))
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    // Modify the SQL query to include the LikeCount column.
+                    string query = "SELECT [dbo].[Post].*, [dbo].[User].*, " +
+                                   "CONVERT(VARBINARY(max), [dbo].[Post].PostPic, 1) AS Base64Image, " +
+                                   "(SELECT COUNT(*) FROM [dbo].[Replies] WHERE [dbo].[Replies].PostId = [dbo].[Post].Id) AS ReplyCount " +
+                                   "FROM [dbo].[Post] " +
+                                   "INNER JOIN [dbo].[User] ON [dbo].[Post].userid = [dbo].[User].id";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable ds = new DataTable();
+                    da.Fill(ds);
+
+                    // Bind the data to the Repeater control.
+                    PostRepeater.DataSource = ds;
+                    PostRepeater.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+
+
+
+        protected void ReplyLink_Click(object sender, EventArgs e)
+        {
+            if (Session["role"].ToString() == "")
+            {
+                Response.Redirect("userlogin.aspx");
+            }
+            LinkButton lb = (LinkButton)sender;
+            string id = lb.CommandArgument;
+            Response.Redirect("replies.aspx?id=" + id);
+        }
+
+        protected void EditLink_Click(object sender, EventArgs e)
+        {
+            LinkButton lb = (LinkButton)sender;
+            string id = lb.CommandArgument;
+            Response.Redirect("editpost.aspx?id=" + id);
+
+        }
+
+        protected void DeleteLink_Click(object sender, EventArgs e)
+        {
+            LinkButton lb = (LinkButton)sender;
+            string id = lb.CommandArgument;
+            int eid = Int32.Parse(id);
+
+            Deletepost(eid);
+        }
+
+        protected void LikeLink_Click(object sender, EventArgs e)
+        {
+            if (Session["role"].ToString() == "")
+            {
+                Response.Redirect("userlogin.aspx");
+            }
+            LinkButton lb = (LinkButton)sender;
+            string id = lb.CommandArgument;
+            int postId = Int32.Parse(id);
+
+            try
+            {
+                // Increment the LikeCount for the post in the Post table.
+                using (SqlConnection con = new SqlConnection(strcon))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE [dbo].[Post] SET LikeCount = LikeCount + 1 WHERE Id = @PostId", con);
+                    cmd.Parameters.AddWithValue("@PostId", postId);
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Refresh the data to reflect the updated like count.
+                RepeterData();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected string GetPostPicUrl(object postPic)
+        {
+            if (postPic != DBNull.Value)
+            {
+                byte[] imageData = (byte[])postPic;
+                string base64String = Convert.ToBase64String(imageData);
+                return "data:image/jpeg;base64," + base64String;
+            }
+            else
+            {
+                // Return a default image URL or an empty string
+                // For example, return a URL to a placeholder image
+                return "url_to_default_image.jpg";
+            }
+        }
+
+
+        protected bool IsImageAvailable(object postPic)
+        {
+            return postPic != DBNull.Value;
+        }
+
+
+        void Deletepost(int eid)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * from [dbo].[Post] where id=" + eid + ";", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count == 1)
+                {
+                    SqlCommand cmd1 = new SqlCommand("DELETE FROM [dbo].[Post]  WHERE id =" + eid + ";", con);
+                    cmd1.ExecuteNonQuery();
+                    Response.Write("<script>alert('Post deleted successfully.');</script>");
+                    RepeterData();
+                }
+                else
+                {
+                    Response.Write("<script>alert('data not exist');</script>");
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+
+        }
+
+        protected void PostRepeater_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+    }
+}
